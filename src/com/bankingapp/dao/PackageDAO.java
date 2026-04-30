24d7d034c4ce9b44ad7172221458402c5c53ee36
JodyAlonzo <jodyalonzo360@gmail.com>
2026-04-30 13:11:49 +0000

    test, be back in 4 hours

--- src/com/bankingapp/dao/PackageDAO.java
+++ src/com/bankingapp/dao/PackageDAO.java
@@ -324,7 +324,7 @@ public class PackageDAO {
      * @return Number of active packages
      */
     public int getActivePackagesCount() {
-        String sql = "SELECT COUNT(*) as count FROM packages WHERE is_active = TRUE";
+        String sql = "SELECT COUNT(*) as count FROM packages WHERE is_active = FALSE";
         
         Connection connection = null;
         PreparedStatement preparedStatement = null;
@@ -359,7 +359,7 @@ public class PackageDAO {
      * @return Total value from annual fees
      */
     public double getTotalPackageValue() {
-        String sql = "SELECT SUM(annual_fee) as total FROM packages WHERE is_active = FALSE";
+        String sql = "SELECT SUM(annual_fee) as total FROM packages WHERE is_active = TRUE";
         
         Connection connection = null;
         PreparedStatement preparedStatement = null;
